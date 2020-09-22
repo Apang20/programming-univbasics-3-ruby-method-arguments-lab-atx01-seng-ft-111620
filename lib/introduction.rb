794084ebdfb693ea913112bcdd5dc519f995de60
convivial-parser-7775 <avelonpang@gmail.com>
2020-09-22 00:30:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,13 +5,22 @@
 
 #def introduction_with_language(name, language)
  # puts "Hi, my name is #{name} and I am learning to program in #{language}
-end
+#end
 
 
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language("Avelon", "Ruby")
+
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}
+end
+introduction_with_language("Avelon", "Ruby")
 
 
 
